3178b137e6ce08976c2b718f920ff00cdc7871f4
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-10-21 10:45:18 +0300

    create componenta

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="main">
+      <div className="graf">
+        <h1>Total</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
